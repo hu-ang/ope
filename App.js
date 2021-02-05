cfe4d28dd9bc3fc8aa0be16863de33db89d7db7d
hu.ang <hu.ang@husky.neu.edu>
2021-02-04 23:44:33 -0500

    lecture app and game

--- App.js
+++ App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { uniq, bad_guesses, word_view, lives_left } from './game';
 import './App.css';
 
 function App() {
+  const [secret, _setSecret] = useState("elephant");
+  const [guesses, setGuesses] = useState([]);
+  const [guess, setGuess] = useState("");
+
+  let view = word_view(secret, guesses);
+  let bads = bad_guesses(secret, guesses);
+  let lives = lives_left(secret, guesses);
+
+  function updateGuess(ev) {
+    let text = ev.target.value;
+    if (text.length > 1) {
+      text = text[0];
+    }
+    setGuess(text);
+  }
+
+  function makeGuess() {
+    setGuesses(uniq(guesses.concat(guess)));
+    setGuess("");
+  }
+
+  function keypress(ev) {
+    if (ev.key == "Enter") {
+      makeGuess();
+    }
+  }
+
+  if (lives <= 0) {
+    return (
+      <div className="App">
+        <h1>Game Over</h1>
+        <p>
+          <button onClick={() => setGuesses([])}>
+            Reset
+          </button>
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Word: {view.join(' ')}</h1>
+      <h1>Guesses: {guesses.join(' ')}</h1>
+      <h1>Bads: {bads.join(' ')}</h1>
+      <h1>Lives: {lives}</h1>
+      <p>
+        <input type="text"
+               value={guess}
+               onChange={updateGuess}
+               onKeyPress={keypress} />
+        <button onClick={makeGuess}>
+          Guess
+        </button>
+      </p>
+      <p>
+        <button onClick={() => setGuesses([])}>
+          Reset
+        </button>
+      </p>
     </div>
   );
+
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
